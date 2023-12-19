172096b8f70802b46b25b450e335d9d8135c0076
AndyKaspe07 <andy.kaspe07@gmail.com>
2023-12-19 16:07:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,38 +5,15 @@ try:
 except ImportError:
     print("No module named google found")
 
-
+# This method gets the fbref player code using the URL 
 def get_player_code(name, surname):
     
     query = name + surname + "fbref"
     
     for i in search(query, tld = "com", num = 1, stop = 1):
         return str(i)[29:37]
-    
 
-
-
-name = input("Enter players' first name") 
-surname = input("Enter players' second name")
-
-code = get_player_code(name, surname)
-
-
-games = int(input("How many games would you like to see the stats for?"))
-today = datetime.date.today()
-thisYear = today.year
-nextYear = thisYear + 1
-
-
-url = "https://fbref.com/en/players/" + code + "/matchlogs/" + str(thisYear) + "-" + str(nextYear) + "/misc/" + name + "-" + surname + "-Match-Logs"
-
-
-
-stats = pd.read_html(url)
-
-DataFrame = stats[0] #extract first table from webpage
-
-
+# Cleans the pandas dataframe that has been passed through 
 def processing(df):
     """
     Pre-Processing function
@@ -55,8 +32,30 @@ def processing(df):
 
     return df
 
+# This method returns the current and next year as a tuple
+def getYear():
+    today = datetime.date.today()
+    this = today.year
+    next = thisYear + 1
+
+    return (str(this), str(next))
+
+# Get inputs
+name = input("Enter players' first name") 
+surname = input("Enter players' second name")
+games = int(input("How many games would you like to see the stats for?"))
+
+code = get_player_code(name, surname)
+thisYear, nextYear = getYear()
+
+url = "https://fbref.com/en/players/" + code + "/matchlogs/" + thisYear + "-" + nextYear + "/misc/" + name + "-" + surname + "-Match-Logs"
+
+
+
+stats = pd.read_html(url)
+DataFrame = stats[0] #extract first table from webpage
 cleaned = processing(DataFrame)
 
+#Output
 print("Here are the last", games, "matches in which ", name, " ", surname, " played 60+ mins:")
-
 print(cleaned.tail(games))
